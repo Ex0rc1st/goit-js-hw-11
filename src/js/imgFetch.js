223c631d0d127c420e892cd2d4a140e5c087da19
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-09-08 16:11:19 +0300

    fixed

--- src/js/imgFetch.js
+++ src/js/imgFetch.js
@@ -7,3 +7,22 @@ export default async function fetchImages(value, page) {
 
   return await axios.get(`${url}${filter}`).then(response => response.data);
 }
+
+
+// export const fetchImages = async (inputValue, pageNr) => {
+//   return await fetch(
+//     `https://pixabay.com/api/?key=29793920-75f6103355f31eb71a4495b72&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+//   )
+//     .then(async response => {
+//       if (!response.ok) {
+//         if (response.status === 404) {
+//           return [];
+//         }
+//         throw new Error(response.status);
+//       }
+//       return await response.json();
+//     })
+//     .catch(error => {
+//       console.error(error);
+//     });
+// };
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import { fetchImages } from './imgFetch';
+import fetchImages from './imgFetch';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
